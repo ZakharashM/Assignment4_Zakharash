2287e7d6e0bdab5e973ff3e300a4dbf0c34c83c2
ZakharashM <mykhailo.zakharash.25@kse.org.ua>
2026-07-20 11:15:38 +0300

    Changed the "N/A"s into 0 in both files + made them int and float

--- my_code.py
+++ my_code.py
@@ -4,4 +4,15 @@ with open("global_sales.csv", "r", encoding="utf-8") as file:
     sales = list(csv.DictReader(file))
 with open("regional_tariffs.json","r", encoding="utf-8") as file:
     regional_tariffs = json.load(file)
+for item in sales:
+    if item["quantity"] == "N/A":
+        item["quantity"] = 0
+    if item["revenue"] == "N/A":
+        item["revenue"] = 0
+    item["quantity"] = int(item["quantity"])
+    item["revenue"] = float(item["revenue"])
+for key,value in regional_tariffs.items():
+    if regional_tariffs[key] == "N/A":
+        regional_tariffs[key] = 0
 print(regional_tariffs)
+print(sales)
